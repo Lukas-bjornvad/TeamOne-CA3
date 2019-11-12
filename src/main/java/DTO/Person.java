6a444538f16875d5fc6109fcdb8aadf494739b41
wormple12 <wormple12@hotmail.com>
2019-11-12 15:21:08 +0100

    fetching from star wars API (example) done and working, DataFetcher created and works.

--- src/main/java/DTO/Person.java
+++ src/main/java/DTO/Person.java
@@ -9,7 +9,7 @@ import java.util.LinkedList;
 
 /**
  *
- * @author Lukas Bjornvad
+ * @author Simon Norup
  */
 public class Person {
     private String name;
@@ -20,115 +20,58 @@ public class Person {
     private String eye_color;
     private String birth_year;
     private String gender;
-    private Object homeworld;
-    private Object species;
-    private LinkedList<Object> starships; 
-    private LinkedList<Object> vehicles; 
-
-
+    private String homeworld;
+    private LinkedList<String> species;
+    private LinkedList<String> starships; 
+    private LinkedList<String> vehicles; 
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public double getHeight() {
         return height;
     }
 
-    public void setHeight(double height) {
-        this.height = height;
-    }
-
     public double getMass() {
         return mass;
-    }
-
-    public void setMass(double mass) {
-        this.mass = mass;
     }
 
     public String getHair_color() {
         return hair_color;
     }
 
-    public void setHair_color(String hair_color) {
-        this.hair_color = hair_color;
-    }
-
     public String getSkin_color() {
         return skin_color;
-    }
-
-    public void setSkin_color(String skin_color) {
-        this.skin_color = skin_color;
     }
 
     public String getEye_color() {
         return eye_color;
     }
 
-    public void setEye_color(String eye_color) {
-        this.eye_color = eye_color;
-    }
-
     public String getBirth_year() {
         return birth_year;
-    }
-
-    public void setBirth_year(String birth_year) {
-        this.birth_year = birth_year;
     }
 
     public String getGender() {
         return gender;
     }
 
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public Object getHomeworld() {
+    public String getHomeworld() {
         return homeworld;
     }
 
-    public void setHomeworld(Object homeworld) {
-        this.homeworld = homeworld;
-    }
-
-    public Object getSpecies() {
+    public LinkedList<String> getSpecies() {
         return species;
     }
 
-    public void setSpecies(Object species) {
-        this.species = species;
-    }
-
-    public LinkedList<Object> getStarships() {
+    public LinkedList<String> getStarships() {
         return starships;
     }
 
-    public void setStarships(LinkedList<Object> starships) {
-        this.starships = starships;
-    }
-
-    public void addStarships(Starship starship) {
-        this.starships.add(starship);
-    }
-
-    public LinkedList<Object> getVehicles() {
+    public LinkedList<String> getVehicles() {
         return vehicles;
     }
-
-    public void setVehicles(LinkedList<Object> vehicles) {
-        this.vehicles = vehicles;
-    }
-
-    public void addVehicles(Vehicle vehicle) {
-        this.vehicles.add(vehicle);
-    }
-
+    
+    
 }
