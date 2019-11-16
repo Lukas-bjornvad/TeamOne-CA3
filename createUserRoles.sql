bfe491c1b5e641ef45bc85273d3d9fbdc9d4a37c
Simon Asholt Norup <wormple12@hotmail.com>
2019-11-16 14:15:36 +0100

    createUserRoles.sql updated

--- createUserRoles.sql
+++ createUserRoles.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `roles` (
+CREATE TABLE IF NOT EXISTS `roles` (
   `role_name` varchar(20) NOT NULL,
   PRIMARY KEY (`role_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
