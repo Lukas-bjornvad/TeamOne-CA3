d580929b3483a1f6b6f144087220d0d15794d27f
Simon Asholt Norup <wormple12@hotmail.com>
2019-11-16 15:20:46 +0100

    openAPI deployed url updated

--- src/main/java/rest/StarWarsResource.java
+++ src/main/java/rest/StarWarsResource.java
@@ -44,7 +44,7 @@ import javax.ws.rs.core.UriInfo;
             ),
             @Server(
                     description = "Server API",
-                    url = "https://www.helvedesmaskine.dk/teamone-ca3"
+                    url = "https://www.helvedesmaskine.dk/TeamOne-CA3"
             )
 
         }
